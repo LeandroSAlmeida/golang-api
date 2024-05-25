b471a7491b320bdc1dda26d10646004226dd69e0
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2024-05-24 21:16:29 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/LeandroSAlmeida/golang-api/router"
+import (
+	"github.com/LeandroSAlmeida/golang-api/config"
+	"github.com/LeandroSAlmeida/golang-api/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
-	//Initialize Router
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
